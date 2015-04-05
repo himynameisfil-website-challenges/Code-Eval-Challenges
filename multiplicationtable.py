3b3b1ca37e43a04c44cf7ee566d601dd6ea10644
himynameisfil <nguye208@gmail.com>
2015-04-05 15:00:37 -0400

    Update multiplicationtable.py

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -1,5 +1,7 @@
-# Code-Eval-Challenges
-challenge problems found on codeeval
+##Status:Solved
+##Problem:https://www.codeeval.com/open_challenges/23/
+##Print out the grade school multiplication table upto 12*12.
+
 for i in range(1,13):
     for j in range(1,13):
         for k in range(4-len(str(i*(j)))):
