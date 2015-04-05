1827567719d61f38474b09e700338671017b698e
himynameisfil <nguye208@gmail.com>
2015-04-05 14:54:14 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,8 @@
-# Code-Eval-Challenges
-# new comment
-challenge problems found on codeeval
+##Status:Solved
+##Problem:https://www.codeeval.com/open_challenges/22/
+##The Fibonacci series is defined as: F(0) = 0; F(1) = 1; F(n) = F(n–1) + F(n–2) when n>1. Given an integer n≥0, print out the F(n).
+
+#The function will determine the n'th fibonacci number using the golden ratio
 import sys
 goldratio = (1 + 5**.5)/2
 antigoldratio = (1 - 5**.5)/2
