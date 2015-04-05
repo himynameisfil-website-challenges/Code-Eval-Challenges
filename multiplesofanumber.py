cfff29c4f849252136a16e6839da0e4768db67cf
himynameisfil <nguye208@gmail.com>
2015-04-05 14:58:49 -0400

    Update multiplesofanumber.py

--- multiplesofanumber.py
+++ multiplesofanumber.py
@@ -1,5 +1,7 @@
-# Code-Eval-Challenges
-challenge problems found on codeeval
+##Status:Solved
+##Problem:https://www.codeeval.com/open_challenges/18/
+##Given numbers x and n, where n is a power of 2, print out the smallest multiple of n which is greater than or equal to x. Do not use division or modulo operator
+
 import sys
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
