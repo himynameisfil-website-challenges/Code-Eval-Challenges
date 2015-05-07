fca7c04f2859167a9a189bb57b7fc134622947b6
himynameisfil <nguye208@gmail.com>
2015-05-07 17:20:32 -0400

    Update setintersections(partially).py

--- setintersections(partially).py
+++ setintersections(partially).py
@@ -1,21 +1,13 @@
 ##Status:Partially completed
 ##Problem:https://www.codeeval.com/open_challenges/30/
 ##Print the intersection of two sets of numbers.
-
+#line 10 is effectively (I'll take a look at the first half of the numbers, see if it's in the second half, if it is. it's a part of this list
 import sys
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
-    if test == "":
-        break
-    x = test.split(";")
-    y = set(x[0].split(","))
-    z = set(x[1].split(","))
-    a = list((y).intersection(z))
-    a.sort()
-    for i in a:
-        print(i, end = "")
-        if i != a[len(a)-1]:
-            print(end = ",")
-    print()
+    if test =="":
+      break
+    lst = [i for i in test.split(";")[0].split(",") if i in test.split(";")[1][:-1].split(",")]
+    print(",".join(lst))
 
 test_cases.close()
