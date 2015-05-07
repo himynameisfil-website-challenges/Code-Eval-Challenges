40159211e76d1996c7d9e4b6644cfc0db12b805c
himynameisfil <nguye208@gmail.com>
2015-05-07 17:21:26 -0400

    Update setintersections(partially).py

--- setintersections(partially).py
+++ setintersections(partially).py
@@ -1,4 +1,4 @@
-##Status:Partially completed
+##Status:solved
 ##Problem:https://www.codeeval.com/open_challenges/30/
 ##Print the intersection of two sets of numbers.
 #line 10 is effectively (I'll take a look at the first half of the numbers, see if it's in the second half, if it is. it's a part of this list
